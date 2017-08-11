9a14e64357c36e985b96121accdca50436b7b19a
crodriguez1a <crodriguez1a@gmail.com>
2017-08-10 23:47:07 -0400

    tree shake bulma-container

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -21,6 +21,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    'ember-bulma': {
+      except: ['bulma-container'] // excludes `bulma-container` https://github.com/open-tux/ember-bulma/issues/78
     }
   };
 
