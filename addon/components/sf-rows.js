cca7ab6f58afb29408e7b790e50c60ea60c01889
Carlos Rodriguez <crodriguez1a@gmail.com>
2017-08-20 17:04:08 -0400

    Prevent mutations on hash passed down

--- addon/components/sf-rows.js
+++ addon/components/sf-rows.js
@@ -1,7 +1,25 @@
 import Component from '@ember/component';
 import layout from '../templates/components/sf-rows';
+import { computed } from 'ember-decorators/object';
+import { assign } from '@ember/polyfills';
 
 export default Component.extend({
   layout,
-  tagName: ''
+  tagName: '',
+
+  /**
+    Create a safe copy of the hash provided
+
+    @property _rows
+    @private
+    @returns {Array}
+  */
+  @computed('rows')
+  _rows(rows) {
+    if (rows && rows.length) {
+      return rows.map((item) => assign({}, item));
+    } else {
+      return [];
+    }
+  }
 });
